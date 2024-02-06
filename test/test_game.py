6fe3fade7f162603afc26be23814428740419fe5
sthudium25 <sam.thudium1@gmail.com>
2024-02-06 11:19:22 -0500

    Set up unit testing with pytest

--- test/test_game.py
+++ test/test_game.py
@@ -25,7 +25,8 @@ class SurvivorWorld(games.Game):
         return False
 
 
-def setup_function():
+@pytest.fixture
+def game_fixture():
     # Set up a reusable game state
     # This contains only the camp, cliffs, beach, and ocean
     camp = things.Location(
@@ -124,10 +125,68 @@ def setup_function():
     return game
 
 
-def test_next_item_id():
+@pytest.fixture
+def player(game_fixture):
+    return game_fixture.player
+
+
+def create_item(name, desc, examine):
+    item = things.Item(name, desc, examine)
+    return item
+
+
+def test_next_item_id(game_fixture):
     next_id = things.Thing.get_current_id()
     assert next_id == 13
 
 
+def test_player_items_and_properties(player):
+    inv = player.inventory.items
+    assert len(inv) == 1
+    assert "torch" in inv
+    torch = inv["torch"][0]
+    assert len(torch.properties) == 3
+    assert "is_lightable" in torch.properties
+    assert torch.get_property("is_lightable")
 
+
+def test_item_property_setup(player):
+    inv = player.view_inventory()
+    print(inv)
+    for _, item in inv.items():
+        for i in item:
+            assert player.is_in_inventory(i.name)
+
+
+def test_inventory_check(player):
+    assert player.inventory.check_inventory("torch")
+
+
+def test_inventory_item_count(player):
+    assert player.inventory.get_quantity("torch") == 1
+
+
+def test_inventory_get_item(player):
+    torch = player.inventory.get_item("torch")
+    assert torch
+    assert isinstance(torch, things.Item)
+    # This should not remove the item from the inventory
+    assert player.inventory.get_quantity("torch") == 1
+
+
+def test_inventory_remove_item(player):
+    torch = player.inventory.remove_item("torch")
+    assert torch
+    assert isinstance(torch, things.Item)
+    assert player.inventory.get_quantity("torch") == 0
+
+
+def test_inventory_get_size(player):
+    assert player.inventory.size() == 1
+    stick = create_item("stick", "a stick", "a long stick")
+    player.add_to_inventory(stick)
+    assert player.inventory.size() == 2
+    assert player.inventory.get_quantity("stick") == 1
+    assert player.is_in_inventory("stick")
+    assert not player.is_in_inventory("phone")
 
