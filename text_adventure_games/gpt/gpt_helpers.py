3d0ec2832897d7054ba7e5841d198ff27b1804fc
sthudium25 <sam.thudium1@gmail.com>
2024-03-15 22:45:19 -0400

    text_adventure_games/gpt/gpt_helpers.py add gpt helper files

--- text_adventure_games/gpt/gpt_helpers.py
+++ text_adventure_games/gpt/gpt_helpers.py
@@ -1,3 +1,4 @@
+import re
 from ..utils.general import set_up_openai_client
 
 def gpt_get_summary_description_of_action(statement, client, model, max_tokens):
@@ -18,3 +19,27 @@ def gpt_get_summary_description_of_action(statement, client, model, max_tokens):
 
     summary_statement = response.choices[0].message.content
     return summary_statement
+
+
+def gpt_get_action_importance(statement, client, model, max_tokens):
+    system = "Gauge the importance of the provided sentence on a scale from 1 to 10, where 1 is mundane and 10 is critical."
+    messages = [{"role": "system", "content": system},
+                {"role": "user", "content": statement}]
+
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=1,
+        max_tokens=max_tokens,
+        top_p=0.5,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    importance_str = response.choices[0].message.content
+    pattern = r"\d+"
+    matches = re.findall(pattern, importance_str)
+    if matches:
+        # Take the first number that it gave us
+        return matches[0]
+    return None
