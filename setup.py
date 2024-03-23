f53006cf2f04b8d6e528116cbf5c3b084a3a7a58
sthudium25 <sam.thudium1@gmail.com>
2024-03-23 12:08:44 -0400

    Add requirements file

--- setup.py
+++ setup.py
@@ -1,26 +1,32 @@
 from setuptools import setup, find_packages
+import os
+import re
 
 from text_adventure_games import __version__
+
+base_game_install_reqs = ['jupyter', 'graphviz']
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open(os.path.join(__file__, "requirements.txt"), 'r') as reqs:
+    install_packages = [req for req in reqs.read().split('\n') if not re.match(r"#\s?", req) and req]
+    install_packages.extend(base_game_install_reqs)
+
 setup(
     name='text adventure games',
     version=__version__,
-    author='Chris Callison-Burch, Jms Dnns',
-    author_email='ccb@upenn.edu',
-    description='A framework for building text based RPGs',
+    author='Generative Agents Framework: Samuel Thudium, Federico Cimini; Base game Framework: Chris Callison-Burch, Jms Dnns',
+    author_email='sam.thudium1@gmail.com; ccb@upenn.edu',
+    # description='A framework for building text based RPGs',
+    description='A framework for building episodic and competitive text-based RPGs with generative agents.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://interactive-fiction-class.org/',
     # license='',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'jupyter',
-        'graphviz',
-    ],
+    install_requires=install_packages,
     extras_require={
         'dev': [
             'black',
