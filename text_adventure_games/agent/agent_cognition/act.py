db87acc0be14b138f597c4fd8e989e0506470812
sthudium25 <sam.thudium1@gmail.com>
2024-04-03 00:42:14 -0400

    Move world/goal into system prompt; add character impressions

--- text_adventure_games/agent/agent_cognition/act.py
+++ text_adventure_games/agent/agent_cognition/act.py
@@ -27,28 +27,37 @@ def act(game, character):
 
     system_prompt = "".join([
         # TODO: replace first line of prompt with more descriptive character summary
+        f"WORLD INFO: {game.world_info}",
         f"You are {character.persona.summary}. ", 
-        "Given the following context of your surroundings, past memories, ",
-        "and relationships with other characters, select a next action that furthers your ",
-        "strategic position in the game. "
+        f"Your current GOALS: {character.goals}. "
+        "Given the context of your environment, past memories, ",
+        "and interpretation of relationships with other characters,",
+        "select a next action that advances your goals or strategy. "
     ])
 
     user_messages = ""
-    # TODO: move game info and goals to the system_prompt?
-    user_messages += f"WORLD INFO: {game.world_info}"
-    user_messages += f"GOALS: {character.goals}. "
-    # TODO: add personal relationships here as well
-    # NOTE: For now we'll limit this to the default latest summary: last 10 memories
-    # TODO: Add "short term memory" nodes by default?
+
+    # Add the theory of mind of agents that this agent has met
+    impression_targets = character.get_characters_in_view(game)
+
+    user_messages += character.impressions.get_impressions_of_chars_in_view(impression_targets)
+
+    # Retrieve the relevant memories to the situation
     user_messages += "These are select MEMORIES in ORDER from LEAST to MOST RELEVANT: " 
     context_list = retrieve(game, character, query=None, n=-1)
 
-    # limit the context length here on the retrieved memories
+    # Add a copy of the existing user message at the end of the memory context list 
+    context_list.append([user_messages[:]])
+
+    # limit the context length here on the retrieved memories + the relationships
     context_list = limit_context_length(context_list, 
                                         max_tokens=GPT4_MAX_TOKENS-ACTION_MAX_OUTPUT, 
                                         tokenizer=game.parser.tokenizer)
-    # Then add these to the user message
-    print(f"passing {len(context_list)} relevant memories to {character.name}")
+    
+    # Then add these to the user message after popping the user messsage off 
+    context_list.pop()
+
+    # print(f"passing {len(context_list)} relevant memories to {character.name}")
     user_messages += "".join([f"{m}\n" for m in context_list])
 
     action_to_take = generate_action(system_prompt, available_actions, user_messages)
