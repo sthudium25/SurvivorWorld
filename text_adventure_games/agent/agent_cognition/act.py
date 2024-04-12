f9749526cffc6657477c41a397f2116dcd57db58
sthudium25 <sam.thudium1@gmail.com>
2024-04-12 14:45:15 -0400

    Add build user/system prompt methods; handle diff cognitive architectures

--- text_adventure_games/agent/agent_cognition/act.py
+++ text_adventure_games/agent/agent_cognition/act.py
@@ -14,62 +14,57 @@ Description: defines how agents select an action given their perceptions and mem
 
 
 # local imports
-from text_adventure_games.gpt.gpt_helpers import gpt_pick_an_option, limit_context_length
+from text_adventure_games.gpt.gpt_helpers import limit_context_length
 from text_adventure_games.utils.general import set_up_openai_client, enumerate_dict_options
 from .retrieve import retrieve
 
 GPT4_MAX_TOKENS = 8192
 ACTION_MAX_OUTPUT = 100
+ACTION_RETRIES = 5
 
 
 def act(game, character):
     available_actions = game.parser.actions
 
-    system_prompt = "".join([
-        # TODO: replace first line of prompt with more descriptive character summary
-        f"WORLD INFO: {game.world_info}",
-        f"You are {character.persona.summary}. ", 
-        f"Your current GOALS: {character.goals}. " #TODO: make it optional
-        "Given the context of your environment, past memories, ",
-        "and interpretation of relationships with other characters, ",
-        "select a next action that advances your goals or strategy. "
-    ])
+    system_prompt = build_system_message(game, character, available_actions)
 
-    user_messages = ""
+    user_prompt = build_user_message(game, character)
 
-    # Add the theory of mind of agents that this agent has met
-    impression_targets = character.get_characters_in_view(game)
-    user_messages += character.impressions.get_multiple_impressions(impression_targets)
-
-    # Retrieve the relevant memories to the situation
-    user_messages += "These are select MEMORIES in ORDER from LEAST to MOST RELEVANT: " 
-    context_list = retrieve(game, character, query=None, n=-1)
-
-    if context_list:
-        # Add a copy of the existing user message at the end of the memory context list 
-        context_list.append(user_messages[:])
-
-        # limit the context length here on the retrieved memories + the relationships
-        context_list = limit_context_length(context_list, 
-                                            max_tokens=GPT4_MAX_TOKENS-ACTION_MAX_OUTPUT, 
-                                            tokenizer=game.parser.tokenizer)
-        
-        # Then add these to the user message after popping the user messsage off 
-        context_list.pop()
-
-        # print(f"passing {len(context_list)} relevant memories to {character.name}")
-        user_messages += "".join([f"{m}\n" for m in context_list])
-
-    action_to_take = generate_action(system_prompt, available_actions, user_messages)
+    action_to_take = generate_action(system_prompt, user_prompt)
     game.logger.debug(f"{character.name} chose to take action: {action_to_take}")
     print(f"{character.name} chose to take action: {action_to_take}")
     return action_to_take
 
-
-def generate_action(system, game_actions, user):
+def generate_action(system_prompt, user_prompt):
     client = set_up_openai_client("Helicone")
 
-    choices_str, options_list = enumerate_dict_options(game_actions, names_only=True)
+    for i in range(ACTION_RETRIES):
+        # print(f"retry {i}/{ACTION_RETRIES} for this character")
+        try:
+            response = client.chat.completions.create(
+                model="gpt-4",
+                messages=[
+                    {"role": "system", "content": system_prompt},
+                    {"role": "user", "content": user_prompt},
+                ],
+                temperature=1,
+                top_p=1,
+                max_tokens=ACTION_MAX_OUTPUT,
+                frequency_penalty=0,
+                presence_penalty=0
+            )
+            
+            out = response.choices[0].message.content
+        except Exception as e:
+            continue
+        else:
+            return out
+    return None
+
+def build_system_message(game, character, game_actions) -> str:
+    system = collect_system_info(game, character)
+    choices_str, _ = enumerate_dict_options(game_actions, names_only=True)
+    
     system += "".join([
         "Using the information provided, generate a short action statement in the present tense from your perspective. ",
         "Be sure to mention any characters you wish to interact with by name. ",
@@ -90,16 +85,55 @@ def generate_action(system, game_actions, user):
         choices_str
     ])
 
-    response = client.chat.completions.create(
-        model="gpt-4",
-        messages=[
-            {"role": "system", "content": system},
-            {"role": "user", "content": user},
-        ],
-        temperature=1,
-        max_tokens=100,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
+    return system
 
-    return response.choices[0].message.content
+def collect_system_info(game, character):
+    previous_round = game.round - 1
+    world_info = game.world_info
+    agent_summary = character.persona.summary  # TODO: update this to match Louis' methodology
+    if character.use_goals:
+        agent_goals = str(character.goals.get_goals(round=previous_round))  # convert the dict to a string
+    else:
+        agent_goals = None
+
+    system = ""
+
+    if world_info:
+        system += f"WORLD INFO: {game.world_info}\n"
+    if agent_summary:
+        system += f"You are {character.persona.summary}.\n"
+    if agent_goals:
+        system += f"Your current GOALS: {character.goals}.\n"
+         
+    system += "".join([
+        "Given the context of your environment, past memories, ",
+        "and interpretation of relationships with other characters,",
+        "select a next action that advances your goals or strategy. "])
+    
+    return system
+
+def build_user_message(game, character):
+    # Add the theory of mind of agents that this agent has met
+    impression_targets = character.get_characters_in_view(game)
+    user_messages = character.impressions.get_multiple_impressions(impression_targets)
+
+    # Retrieve the relevant memories to the situation
+    user_messages += "These are select MEMORIES in ORDER from LEAST to MOST RELEVANT: " 
+    context_list = retrieve(game, character, query=None, n=-1)
+
+    if context_list:
+        # Add a copy of the existing user message at the end of the memory context list 
+        context_list.append(user_messages[:])
+
+        # limit the context length here on the retrieved memories + the relationships
+        context_list = limit_context_length(context_list, 
+                                            max_tokens=GPT4_MAX_TOKENS-ACTION_MAX_OUTPUT, 
+                                            tokenizer=game.parser.tokenizer)
+        
+        # Then add these to the user message after popping the user messsage off 
+        context_list.pop()
+
+        # print(f"passing {len(context_list)} relevant memories to {character.name}")
+        user_messages += "".join([f"{m}\n" for m in context_list])
+    
+    return user_messages
