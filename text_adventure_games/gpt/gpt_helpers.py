5300309a581d1d6cb0740ae38a8250a8fc6c337a
Kyle <kyle.sullivan27@gmail.com>
2024-04-14 23:16:07 -0400

    Reflection Update

--- text_adventure_games/gpt/gpt_helpers.py
+++ text_adventure_games/gpt/gpt_helpers.py
@@ -213,18 +213,20 @@ def gpt_pick_an_option(instructions, options, input_str):
         return None
 
 
-def limit_context_length(history, max_tokens, max_turns=1000, tokenizer=None):
+def limit_context_length(history, max_tokens, max_turns=1000, tokenizer=None, keep_most_recent=True):
     """
     This method limits the length of the command_history 
     to be less than the max_tokens and less than max_turns. 
-    The least recent messages are disregarded from the context. 
-    This function is non-destructive and doesn't modify command_history.
+    The least recent messages are disregarded from the context unless
+    keep_most_recent is changed to False. This function is non-destructive
+    and doesn't modify command_history.
 
     Args:
         history (_type_): _description_
         max_tokens (_type_): _description_
         max_turns (int, optional): _description_. Defaults to 1000.
         tokenizer (_type_, optional): _description_. Defaults to None.
+        keep_most_recent (bool, optional): If True, trim from the beginning values. 
 
     Raises:
         TypeError: _description_
@@ -242,33 +244,23 @@ def limit_context_length(history, max_tokens, max_turns=1000, tokenizer=None):
     
     if len(history) > 0:
         if isinstance(history[0], dict):
-            # this indicates that we're parsing ChatMessages, so should extract the "content" string
-            
-            # original method
-            # extract = lambda x: x["content"]
-
-            # new method
-            # pad with 3 tokens and include both role & content
+            # this indicates that we're parsing ChatMessages, so extract the "content" and "role" strings
+            # pad with 3 tokens and include both content & role
             extract = lambda x: get_prompt_token_count(content=x["content"], role=x["role"], pad_reply=False, tokenizer=tokenizer)
             
             # each reply carries 3 tokens via "<|start|>assistant<|message|>" that need to be added once
             total_tokens += 3
         elif isinstance(history[0], str):
             # this indicates that we're parsing a list of strings
-
-            # original method
-            # extract = lambda x: x
-
-            # new method
             extract = lambda x: len(tokenizer.encode(x))
         else:
             raise TypeError("Elements in history must be either dict or str")
         
-    for message in reversed(history):
-        # original version
-        # msg_tokens = len(tokenizer.encode(extract(message)))
+    # reverse the list if we are keeping only the most recent items
+    if keep_most_recent:
+        history.reverse()
 
-        # new version
+    for message in history:
         msg_tokens = extract(message)
         
         if total_tokens + msg_tokens > max_tokens:
@@ -278,7 +270,11 @@ def limit_context_length(history, max_tokens, max_turns=1000, tokenizer=None):
         if len(limited_history) >= max_turns:
             break
 
-    return list(reversed(limited_history))
+    # reverse the new list (back to normal) if we are keeping only the most recent items
+    if keep_most_recent:
+        limited_history.reverse()
+
+    return list(limited_history)
 
 
 def get_prompt_token_count(content=None, role=None, pad_reply=False, tokenizer=None):
@@ -288,8 +284,13 @@ def get_prompt_token_count(content=None, role=None, pad_reply=False, tokenizer=N
     utilized by GPT's API.
 
     Args:
-        content (str): the prompt content
-        role (str): the prompt role
+        content (str or list of strings): the prompt content; if passing
+                                          a list of strings, this returns
+                                          only the token count of the list
+                                          (not accounting for any padding)
+        role (str): the prompt role; if None, it is processed without the
+                    GPT message padding - essentially just a plain token
+                    counter if pad_reply is False
         pad_reply (bool): True adds a padding to account for GPT's reply
                           being primed with <|start|>assistant<|message|>.
                           GPT only adds one reply primer for the entire
@@ -306,29 +307,39 @@ def get_prompt_token_count(content=None, role=None, pad_reply=False, tokenizer=N
         TypeError: _description_
 
     Returns:
-        _type_: _description_
+        int: number of tokens
     """
 
-    if not isinstance(content, str):
-        raise TypeError("content must be a string, not ", type(content))
-    if not isinstance(role, str):
+    if content is not None and not isinstance(content, str) and not isinstance(content, list):
+        raise TypeError("content must be a string or list, not ", type(content))
+    if content is not None and isinstance(content, list) and (len(content) != 0 and not isinstance(content[0], str)):
+        raise TypeError("content list must contain strings, not ", type(content[0]))
+    if role is not None and not isinstance(role, str):
         raise TypeError("role must be a string, not ", type(role))
     
     if not tokenizer:
         tokenizer = tiktoken.get_encoding("cl100k_base")
 
-    # pad messages with 3 tokens to account for GPT content/role JSON structure
-    token_count = 3
+    # initialize token count to 0
+    token_count = 0
 
-    # if accounting for GPT's reply being primed with <|start|>assistant<|message|>
+    # if accounting for GPT's reply being primed with <|start|>assistant<|message|>, add 3
     if pad_reply:
         token_count += 3
 
-    # if checking message content
-    if content:
+    # if checking string message content
+    if content and isinstance(content, str):
         token_count += len(tokenizer.encode(content))
+
+    # if checking list of strings message content
+    elif content and isinstance(content, list):
+        for c in content:
+            token_count += len(tokenizer.encode(c))
+
     # if checking message role
     if role:
+        # pad messages with 3 tokens to account for GPT's content/role JSON structure
+        token_count += 3
         token_count += len(tokenizer.encode(role))
 
     return token_count
