89aa3525129e8274602fd615bd9733a2fb81d1db
sthudium25 <sam.thudium1@gmail.com>
2024-04-17 19:12:11 -0400

    Add action prompts

---
+++ text_adventure_games/assets/prompts/act_prompts.py
@@ -0,0 +1,20 @@
+action_system_mid = """
+Given the context of your environment, past memories, and interpretation of relationships with other characters,
+select a next action that advances your goals or strategy. 
+"""
+
+action_system_end = """
+\nUsing the information provided, generate a short action statement in the present tense from your perspective.
+Be sure to mention any characters you wish to interact with by name. 
+Examples could be:\nGo outside to the garden.
+Talk to Tom about his strategy 
+Pick up the stone from the ground
+Give your food to the guard
+Climb up the tree\n
+Notes to keep in mind:
+You can only use items that are in your possesion. 
+If you want to go somewhere, state the direction or the location in which you want to travel. 
+Actions should be atomic, not general and should interact with your immediate environment. 
+Aim to keep action statements to 10 words or less. 
+Here is list of valid action verbs to use:\n'
+"""
