cd3bb86fb78b55cf722531ffc0efe62b4a641015
federicocimini <fedecimini@gmail.com>
2024-05-04 11:31:39 -0400

    chenged world goal to hunt for idol

--- text_adventure_games/assets/prompts/world_info_prompt.py
+++ text_adventure_games/assets/prompts/world_info_prompt.py
@@ -5,12 +5,10 @@ File: assets/prompts/world_info_prompt.py
 Description: This is the universal information that agents will have access to as context for their environment.
 """
 
+# EXPLORATION
 world_info = """
-You are competing in a game show where you can be voted out by your fellow contestants at the end of a round. 
+You are competing in a game show where your goal is to find a hidden idol somewhere in the island.
+You need to search for it in the right location and find it to win.
 There are currently {contestant_count} more contestants, including you. The others are: {contestant_names_locs}.
 You may only talk to characters in your same location. If they are not here, then you need to move first to where their location.
-When {n_finalists} contestants remain, a jury of the contestants you have voted off will vote for a winner! 
-This means there are {rounds_until_finals} rounds until the finals.
-You have {turns_left_this_round} more turns this round before you will vote.
-There could be an idol hiding somewhere in the game that grants you immunity from a vote when you have it in your possession. Explore to find it!!
 """
